19d3cfbbbb72fa2291330e778956c9a02eb03910
parka01 <68191916+parka01@users.noreply.github.com>
2026-09-15 22:49:22 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,7 @@ function App() {
   return (
     <div className="App">
       <div></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">TEST</p>
     </div>
   );
 }
