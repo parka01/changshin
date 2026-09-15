973a96afa1d97ccdcbcec52bf2e82ed078ba9621
parka01 <68191916+parka01@users.noreply.github.com>
2026-09-15 23:06:47 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div></div>
-      <p className="read-the-docs">TEST</p>
+      <p className="read-the-docs">TEST TEST</p>
     </div>
   );
 }
